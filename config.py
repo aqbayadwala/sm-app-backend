22b8bfebe129f5b92eaef0736560ce1bf517349a
Adnan Bayadwala <cadbay53@gmail.com>
2024-11-22 23:26:59 +0530

    debugging auth-check

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+# for local machine
 class Config:
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
@@ -14,6 +15,26 @@ class Config:
     JWT_TOKEN_LOCATION = ["headers", "cookies", "json", "query_string"]
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") or "secret-key"
     JWT_COOKIE_SECURE = False
-    JWT_COOKIE_SAMESITE = "LAX"
+    JWT_COOKIE_SAMESITE = None
     JWT_COOKIE_HTTPONLY = True
     JWT_COOKIE_CSRF_PROTECT = False
+
+
+"""
+
+
+# for production
+class Config:
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "sm_app.db")
+    JWT_TOKEN_LOCATION = ["headers", "cookies", "json", "query_string"]
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") or "secret-key"
+    JWT_COOKIE_SECURE = False
+    JWT_COOKIE_SAMESITE = None
+    JWT_COOKIE_HTTPONLY = True
+    JWT_COOKIE_CSRF_PROTECT = False
+
+
+"""
